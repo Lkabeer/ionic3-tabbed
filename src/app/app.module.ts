dd62e288e8ff8b903dc5733fd878477d031c3e60
L kabeer <ahmed@Lkabeer.com>
2017-12-10 10:55:52 +0200

    version 1.4 Using the Card Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,17 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
+import { HttpModule } from "@angular/http";
 
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { ProductDetailPage } from "../pages/product-detail/product-detail";
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { ProductProvider } from '../providers/product/product';
 
 @NgModule({
   declarations: [
@@ -17,10 +20,12 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    ProductDetailPage
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -29,12 +34,14 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    ProductDetailPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    ProductProvider
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http } from "@angular/http";
+import "rxjs/add/operator/map";
+
+import { ProductProvider } from "../../providers/product/product";
+
+import { ProductDetailPage } from "../product-detail/product-detail";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public allProducts = [];
 
-  constructor(public navCtrl: NavController) {
+  constructor(private productProvider: ProductProvider, private http: Http, public navCtrl: NavController) {
 
+  }
+
+  ionViewDidLoad() {
+    this.productProvider.getProducts()
+      .subscribe((response) => {
+        this.allProducts = response;
+      });
+  }
+
+  goToProductDetailsPage(product) {
+    this.navCtrl.push(ProductDetailPage, {
+      productDetails: product
+    });
   }
 
 }
